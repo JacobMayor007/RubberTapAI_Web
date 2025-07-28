c2cf74bae70e123a544528838c9a8281b1b5206d
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-07-28 21:09:46 +0800

    change-password commit

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Verification />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="*" element={<Navigate to="https://www.google.com" />} />
       </Routes>
     </BrowserRouter>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; 
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    react(),  
+    react(),
     tailwindcss(),
   ],
+  base: '/', 
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true, 
+    open: true 
+  }
 });
